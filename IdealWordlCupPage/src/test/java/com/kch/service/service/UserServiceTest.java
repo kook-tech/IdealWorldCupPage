f25e11cba9ab89b3ad11656529d9283fcaf04d1a
DESKTOP-TR1IQBS\sky <knkn9436@naver.com>
2023-05-11 16:49:11 +0900

    Feat. H2 DB 연결을 통해 테이블 생성

---
+++ IdealWordlCupPage/src/test/java/com/kch/service/service/UserServiceTest.java
@@ -0,0 +1,41 @@
+package com.kch.service.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+//    @Autowired
+//    UserService userService;
+    @Test
+    @DisplayName("유저 생성이 올바르게 되는지 테스트")
+    void createUser() {
+        //given
+        //when
+        //then
+    }
+
+    @Test
+    void getUserByUserId() {
+        //given
+        //when
+        //then
+    }
+
+    @Test
+    void updateUser() {
+        //given
+        //when
+        //then
+    }
+
+    @Test
+    void deleteUserByUserId() {
+        //given
+        //when
+        //then
+    }
+}
